0a0fe3bab2a3a242ba9835ffd89f525f381d9848
luis cano <luis.cano@gmail.com>
2023-06-23 19:15:18 -0700

    update db schema based on conversation with Mark, Tina and Patrick

--- Tws.SurveyMonkey.Data/SQL/CreateDB.sql
+++ Tws.SurveyMonkey.Data/SQL/CreateDB.sql
@@ -1,157 +1,168 @@
-DROP TABLE IF EXISTS [dbo].[SMChoice]
+IF NOT EXISTS (SELECT * FROM sys.tables t  
+				JOIN sys.schemas s ON (t.schema_id = s.schema_id) 
+				WHERE s.name = 'dbo' AND t.name = 'SMSurvey')
+	CREATE TABLE [dbo].[SMSurvey](
+		[Id] BIGINT NOT NULL IDENTITY (1,1),
+		[SurveyEntityId] [nvarchar](50) NOT NULL,
+		[Title] [nvarchar](100) NOT NULL,
+		[Category] [nvarchar](200) NULL,
+		[QuestionCount] [int] NOT NULL,
+		[PageCount] [int] NOT NULL,
+		[ResponseCount] [int] NOT NULL,
+		[CreatedDate] [datetime2](7) NOT NULL,
+		[ModifiedDate] [datetime2](7) NOT NULL
+		CONSTRAINT PK_SMSurvey_Id PRIMARY KEY (Id)
+	)
 GO
 
-DROP TABLE IF EXISTS [dbo].[SMQuestion]  
-GO
-
-DROP TABLE IF EXISTS [dbo].[SMPage]
-GO
-
-DROP TABLE IF EXISTS [dbo].[SMRecipient]
-GO
-
-DROP TABLE IF EXISTS [dbo].[SMCollector]
-GO
-
-DROP TABLE IF EXISTS [dbo].[SMSurvey]
-GO
-
-DROP TABLE IF EXISTS [dbo].[SMResponse]
+IF EXISTS(SELECT * FROM sys.indexes WHERE object_id = object_id('dbo.SMSurvey') AND NAME ='UNCI_SurveyEntityId')
+	CREATE UNIQUE NONCLUSTERED INDEX UNCI_SurveyEntityId ON [dbo].[SMSurvey] ([SurveyEntityId] ASC) 
 GO
 
 
-
-CREATE TABLE [dbo].[SMSurvey](
-	[Id] BIGINT NOT NULL IDENTITY (1,1),
-	[SurveyEntityId] [varchar](50) NOT NULL,
-	[Title] [varchar](100) NOT NULL,
-	[Category] [varchar](200) NULL,
-	[QuestionCount] [int] NOT NULL,
-	[PageCount] [int] NOT NULL,
-	[ResponseCount] [int] NOT NULL,
-	[CreatedDate] [datetime2](7) NOT NULL,
-	[ModifiedDate] [datetime2](7) NOT NULL
-	CONSTRAINT PK_SMSurvey_Id PRIMARY KEY (Id)
-)
-GO
-
-CREATE UNIQUE NONCLUSTERED INDEX UNCI_SurveyEntityId
-ON [dbo].[SMSurvey] ([SurveyEntityId] ASC)
-GO
-
+IF NOT EXISTS (SELECT * FROM sys.tables t  
+				JOIN sys.schemas s ON (t.schema_id = s.schema_id) 
+				WHERE s.name = 'dbo' AND t.name = 'SMCollector')
+BEGIN
 CREATE TABLE [dbo].[SMCollector](
 	[Id] BIGINT NOT NULL IDENTITY (1,1),
-	[CollectorEntityId] [varchar](50) NOT NULL,
+	[CollectorEntityId] [nvarchar](50) NOT NULL,
 	[SMSurveyId] BIGINT NOT NULL,
-	[Name] [varchar](200) NOT NULL,
-	[Type] [varchar](20) NOT NULL,
-	[Email] [varchar](50) NOT NULL,
+	[Name] [nvarchar](200) NOT NULL,
+	[Type] [nvarchar](50) NOT NULL,
+	[Email] [nvarchar](100) NOT NULL,
 	CONSTRAINT PK_SMCollector_Id PRIMARY KEY (Id)
 )
+ALTER TABLE [dbo].[SMCollector]  WITH CHECK ADD FOREIGN KEY([SMSurveyId])
+REFERENCES [dbo].[SMSurvey] ([Id]) ON DELETE CASCADE
+
+END
 GO
 
+IF EXISTS(SELECT * FROM sys.indexes WHERE object_id = object_id('dbo.SMCollector') AND NAME ='UNCI_CollectorEntityId')
 CREATE UNIQUE NONCLUSTERED INDEX UNCI_CollectorEntityId
 ON [dbo].[SMCollector] ([CollectorEntityId] ASC)
 GO
 
-ALTER TABLE [dbo].[SMCollector]  WITH CHECK ADD FOREIGN KEY([SMSurveyId])
-REFERENCES [dbo].[SMSurvey] ([Id]) ON DELETE CASCADE
+
+IF NOT EXISTS (SELECT * FROM sys.tables t  
+				JOIN sys.schemas s ON (t.schema_id = s.schema_id) 
+				WHERE s.name = 'dbo' AND t.name = 'SMRecipient')
+BEGIN
+	CREATE TABLE [dbo].[SMRecipient](
+		[Id] BIGINT NOT NULL IDENTITY (1,1),
+		[RecipientEntityId] [nvarchar](50) NOT NULL,
+		[SMCollectorId] BIGINT NOT NULL,
+		[Email] [nvarchar](100) NOT NULL,
+		[PhoneNumber] [nvarchar](50) NULL,
+		CONSTRAINT PK_SMRecipient_Id PRIMARY KEY (Id)
+	)
+
+	ALTER TABLE [dbo].[SMRecipient]  WITH CHECK ADD FOREIGN KEY([SMCollectorId])
+	REFERENCES [dbo].[SMCollector] ([Id]) ON DELETE CASCADE
+END
 GO
 
-CREATE TABLE [dbo].[SMRecipient](
-	[Id] BIGINT NOT NULL IDENTITY (1,1),
-	[RecipientEntityId] [varchar](50) NOT NULL,
-	[SMCollectorId] BIGINT NOT NULL,
-	[Email] [varchar](50) NOT NULL,
-    [PhoneNumber] [varchar](50) NULL,
-	CONSTRAINT PK_SMRecipient_Id PRIMARY KEY (Id)
-)
+IF NOT EXISTS (SELECT * FROM sys.tables t  
+				JOIN sys.schemas s ON (t.schema_id = s.schema_id) 
+				WHERE s.name = 'dbo' AND t.name = 'SMPage')
+BEGIN
+		CREATE TABLE [dbo].[SMPage](
+		[Id] BIGINT NOT NULL IDENTITY (1,1),
+		[PageEntityId] [nvarchar](50) NOT NULL,
+		[SMSurveyId] BIGINT NOT NULL,
+		[Title] [nvarchar](200) NOT NULL,
+		[Description] [nvarchar](200) NULL,
+		[QuestionCount] [int] NOT NULL,
+		[Order] [int] NOT NULL,
+		CONSTRAINT PK_SMPage_Id PRIMARY KEY (Id)
+	)
+
+	ALTER TABLE [dbo].[SMPage]  WITH CHECK ADD FOREIGN KEY([SMSurveyId])
+	REFERENCES [dbo].[SMSurvey] ([Id]) ON DELETE CASCADE
+END
 GO
 
-ALTER TABLE [dbo].[SMRecipient]  WITH CHECK ADD FOREIGN KEY([SMCollectorId])
-REFERENCES [dbo].[SMCollector] ([Id]) ON DELETE CASCADE
+IF NOT EXISTS (SELECT * FROM sys.tables t  
+				JOIN sys.schemas s ON (t.schema_id = s.schema_id) 
+				WHERE s.name = 'dbo' AND t.name = 'SMQuestion')
+BEGIN
+	CREATE TABLE [dbo].[SMQuestion](
+		[Id] BIGINT NOT NULL IDENTITY (1,1),
+		[QuestionEntityId] [nvarchar](50) NOT NULL,
+		[SMPageId] BIGINT NOT NULL,
+		[QuestionType] [nvarchar](200) NULL,
+		[Question] [nvarchar](MAX) NULL,
+		[Order] [int] NOT NULL,
+		CONSTRAINT PK_SMQuestion_Id PRIMARY KEY (Id)
+	)
+
+	ALTER TABLE [dbo].[SMQuestion]  WITH CHECK ADD FOREIGN KEY([SMPageId])
+	REFERENCES [dbo].[SMPage] ([Id]) ON DELETE CASCADE
+END
 GO
 
-CREATE TABLE [dbo].[SMPage](
-	[Id] BIGINT NOT NULL IDENTITY (1,1),
-	[PageEntityId] [varchar](50) NOT NULL,
-	[SMSurveyId] BIGINT NOT NULL,
-	[Title] [varchar](200) NOT NULL,
-	[Description] [varchar](200) NULL,
-	[QuestionCount] [int] NOT NULL,
-	[Order] [int] NOT NULL,
-	CONSTRAINT PK_SMPage_Id PRIMARY KEY (Id)
-)
+IF NOT EXISTS (SELECT * FROM sys.tables t  
+				JOIN sys.schemas s ON (t.schema_id = s.schema_id) 
+				WHERE s.name = 'dbo' AND t.name = 'SMChoice')
+BEGIN
+	CREATE TABLE [dbo].[SMChoice](
+		[Id] BIGINT NOT NULL IDENTITY (1,1),
+		[ChoiceEntityId] [nvarchar](50) NOT NULL,
+		[SMQuestionId] BIGINT NOT NULL,
+		[Name] [nvarchar](200) NULL,
+		[Order] [int] NOT NULL,
+		CONSTRAINT PK_SMChoice_Id PRIMARY KEY (Id)
+	)
+
+	ALTER TABLE [dbo].[SMChoice]  WITH CHECK ADD FOREIGN KEY([SMQuestionId])
+	REFERENCES [dbo].[SMQuestion] ([Id])
+END
 GO
 
-ALTER TABLE [dbo].[SMPage]  WITH CHECK ADD FOREIGN KEY([SMSurveyId])
-REFERENCES [dbo].[SMSurvey] ([Id]) ON DELETE CASCADE
+IF NOT EXISTS (SELECT * FROM sys.tables t  
+				JOIN sys.schemas s ON (t.schema_id = s.schema_id) 
+				WHERE s.name = 'dbo' AND t.name = 'SMResponse')
+BEGIN
+	  CREATE TABLE [dbo].[SMResponse](
+		[Id] BIGINT NOT NULL IDENTITY (1,1),
+		[ResponseEntityId] [nvarchar](50) NOT NULL,
+		[Status] [nvarchar](20) NOT NULL,
+		[IpAddress] [nvarchar](20) NOT NULL,
+		[SurveyEntityId] [nvarchar](50) NOT NULL,
+		[CollectorEntityId] [nvarchar](50) NOT NULL,
+		[RecipientEntityId] [nvarchar](50) NOT NULL,
+		[PageEntityId] [nvarchar](50) NOT NULL,
+		[QuestionEntityId] [nvarchar](50) NOT NULL,
+		[ChoiceEntityId] [nvarchar](50) NULL,
+		[Text] [nvarchar](MAX) NULL,
+		CONSTRAINT PK_SMResponse_Id PRIMARY KEY (Id)
+	)
+
+END
 GO
 
-CREATE TABLE [dbo].[SMQuestion](
-	[Id] BIGINT NOT NULL IDENTITY (1,1),
-	[QuestionEntityId] [varchar](50) NOT NULL,
-	[SMPageId] BIGINT NOT NULL,
-	[QuestionType] [varchar](200) NULL,
-	[Question] [varchar](200) NULL,
-	[Order] [int] NOT NULL,
-	CONSTRAINT PK_SMQuestion_Id PRIMARY KEY (Id)
-)
-GO
-
-ALTER TABLE [dbo].[SMQuestion]  WITH CHECK ADD FOREIGN KEY([SMPageId])
-REFERENCES [dbo].[SMPage] ([Id]) ON DELETE CASCADE
-GO
-
-CREATE TABLE [dbo].[SMChoice](
-	[Id] BIGINT NOT NULL IDENTITY (1,1),
-	[ChoiceEntityId] [varchar](50) NOT NULL,
-	[SMQuestionId] BIGINT NOT NULL,
-	[Name] [varchar](200) NULL,
-	[Order] [int] NOT NULL,
-	CONSTRAINT PK_SMChoice_Id PRIMARY KEY (Id)
-)
-GO
-
-ALTER TABLE [dbo].[SMChoice]  WITH CHECK ADD FOREIGN KEY([SMQuestionId])
-REFERENCES [dbo].[SMQuestion] ([Id])
-GO
-
-
-  CREATE TABLE [dbo].[SMResponse](
-	[Id] BIGINT NOT NULL IDENTITY (1,1),
-	[ResponseEntityId] [varchar](50) NOT NULL,
-	[Status] [varchar](20) NOT NULL,
-	[IpAddress] [varchar](20) NOT NULL,
-	[SurveyEntityId] [varchar](50) NOT NULL,
-	[CollectorEntityId] [varchar](50) NOT NULL,
-	[RecipientEntityId] [varchar](50) NOT NULL,
-	[PageEntityId] [varchar](50) NOT NULL,
-	[QuestionEntityId] [varchar](50) NOT NULL,
-	[ChoiceEntityId] [varchar](50) NULL,
-	[Text] [varchar](1000) NULL,
-	CONSTRAINT PK_SMResponse_Id PRIMARY KEY (Id)
-)
-GO
-
-
-  CREATE TABLE [dbo].[SMMessage](
-	[Id] BIGINT NOT NULL IDENTITY (1,1),
-	[SMCollectorId] BIGINT NOT NULL,
-	[MessageEntityId] [varchar](50) NOT NULL,
-	[Status] [varchar](20) NOT NULL,
-	[IsScheduled] BIT NOT NULL,
-	[EmbedFirstQuestion] BIT NOT NULL,
-	[Subject] [varchar](500) NOT NULL,
-	[Body] [varchar](2000) NOT NULL,
-	[Type] [varchar](20) NOT NULL,
-	[ScheduledDate] [datetime2](7) NOT NULL,
+IF NOT EXISTS (SELECT * FROM sys.tables t  
+				JOIN sys.schemas s ON (t.schema_id = s.schema_id) 
+				WHERE s.name = 'dbo' AND t.name = 'SMMessage')
+BEGIN
+	  CREATE TABLE [dbo].[SMMessage](
+		[Id] BIGINT NOT NULL IDENTITY (1,1),
+		[SMCollectorId] BIGINT NOT NULL,
+		[MessageEntityId] [nvarchar](50) NOT NULL,
+		[Status] [nvarchar](20) NOT NULL,
+		[IsScheduled] BIT NOT NULL,
+		[EmbedFirstQuestion] BIT NOT NULL,
+		[Subject] [nvarchar](500) NOT NULL,
+		[Body] [nvarchar](2000) NULL,
+		[Type] [nvarchar](50) NOT NULL,
+		[ScheduledDate] [datetime2](7) NOT NULL,
 
 	
-	CONSTRAINT PK_SMCollectorMessage_Id PRIMARY KEY (Id)
-)
-GO
+		CONSTRAINT PK_SMCollectorMessage_Id PRIMARY KEY (Id)
+	)
 
-ALTER TABLE [dbo].[SMMessage]  WITH CHECK ADD FOREIGN KEY([SMCollectorId])
-REFERENCES [dbo].[SMCollector] ([Id])
-GO
+	ALTER TABLE [dbo].[SMMessage]  WITH CHECK ADD FOREIGN KEY([SMCollectorId])
+	REFERENCES [dbo].[SMCollector] ([Id])
+
+END
---
+++ Tws.SurveyMonkey.Data/SQL/DeleteAndDropTables.sql
@@ -0,0 +1,51 @@
+DELETE FROM[dbo].[SMChoice]
+GO
+
+DELETE FROM [dbo].[SMQuestion]
+GO
+
+DELETE FROM [dbo].[SMPage]
+GO
+
+DELETE FROM [dbo].[SMRecipient]
+GO
+
+DELETE FROM [dbo].[SMCollector]
+GO
+
+DELETE FROM [dbo].[SMSurvey]
+GO
+
+DELETE FROM [dbo].[SMResponse]
+go
+
+DELETE FROM DBO.SMMessage
+go
+
+
+DROP TABLE IF EXISTS [dbo].[SMResponse]
+go
+
+DROP TABLE IF EXISTS [dbo].[SMMessage]
+GO
+
+DROP TABLE IF EXISTS [dbo].[SMChoice]
+GO
+
+DROP TABLE IF EXISTS [dbo].[SMQuestion]
+GO
+
+DROP TABLE IF EXISTS [dbo].[SMPage]
+GO
+
+DROP TABLE IF EXISTS [dbo].[SMRecipient]
+GO
+
+DROP TABLE IF EXISTS [dbo].[SMCollector]
+GO
+
+DROP TABLE IF EXISTS [dbo].[SMSurvey]
+GO
+
+
+
