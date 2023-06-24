ff1c7b61ef1b5cdb59bb133113fa22262b24ae54
luis cano <luis.cano@gmail.com>
2023-06-23 19:33:49 -0700

    change column name from body to htmlbody

--- Tws.SurveyMonkey.Data/Data/Entities/SMMessage.cs
+++ Tws.SurveyMonkey.Data/Data/Entities/SMMessage.cs
@@ -9,7 +9,7 @@ namespace Tws.SurveyMonkey.Data.Data
 {
     public class SMMessage : IEntityId, ISMObjectId
     {
-        public SMMessage(long id, string messageEntityId, long smCollectorId, string status, bool isScheduled, bool  embedFirstQuestion, string subject, string body, string type, DateTime scheduledDate)
+        public SMMessage(long id, string messageEntityId, long smCollectorId, string status, bool isScheduled, bool  embedFirstQuestion, string subject, string htmlBody, string type, DateTime scheduledDate)
         {
             Id = id;
             ObjectId = messageEntityId;
@@ -17,12 +17,12 @@ namespace Tws.SurveyMonkey.Data.Data
             Status = status; 
             IsScheduled = isScheduled;
             EmbedFirstQuestion = embedFirstQuestion; 
-            Subject = subject; 
-            Body = body; 
+            Subject = subject;
+            HtmlBody = htmlBody; 
             Type = type;
             ScheduledDate = scheduledDate;
         }
-        public SMMessage(string messageEntityId, long smCollectorId, string status, bool isScheduled, bool embedFirstQuestion, string subject, string body, string type, DateTime scheduledDate)
+        public SMMessage(string messageEntityId, long smCollectorId, string status, bool isScheduled, bool embedFirstQuestion, string subject, string htmlBody, string type, DateTime scheduledDate)
         {
             Id = default;
             ObjectId = messageEntityId;
@@ -31,7 +31,7 @@ namespace Tws.SurveyMonkey.Data.Data
             IsScheduled = isScheduled;
             EmbedFirstQuestion = embedFirstQuestion;
             Subject = subject;
-            Body = body;
+            HtmlBody = htmlBody;
             Type = type;
             ScheduledDate = scheduledDate;
         }
@@ -45,7 +45,7 @@ namespace Tws.SurveyMonkey.Data.Data
         public bool IsScheduled { get; set; }
         public bool EmbedFirstQuestion { get; set; }
         public string Subject { get; set; }
-        public string Body { get; set; }
+        public string HtmlBody { get; set; }
         public string Type { get; set; }
         public DateTime ScheduledDate { get; set; }
     }
